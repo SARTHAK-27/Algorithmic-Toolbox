78c663f3081416d9ed5b39aceb54d8cd8dbc672d
SARTHAK-27 <sarthakasthana2014@gmail.com>
2020-05-03 19:55:22 +0530

    week1

--- max_pairwise_product.cpp
+++ max_pairwise_product.cpp
@@ -1,29 +1,47 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+using namespace std;
 
-int MaxPairwiseProduct(const std::vector<int>& numbers) {
+/*int MaxPairwiseProduct(const vector<int>& numbers) {
     int max_product = 0;
     int n = numbers.size();
 
     for (int first = 0; first < n; ++first) {
         for (int second = first + 1; second < n; ++second) {
-            max_product = std::max(max_product,
+            max_product = max(max_product,
                 numbers[first] * numbers[second]);
         }
     }
 
     return max_product;
 }
+*/
+long long MaxPairwiseProductFast(const vector<int>& numbers){
+    int n = numbers.size();
+
+    int max_index1 = -1;
+    for(int i=0;i<n;++i){
+        if((max_index1 == -1) || (numbers[i] > numbers[max_index1]))
+        max_index1 = i;
+    }
+
+    int max_index2 = -1;
+    for(int j=0;j<n;++j){
+        if((j != max_index1) && ((max_index2 == -1) || (numbers[j] > numbers[max_index2])))
+        max_index2 = j;
+    }
+    return ((long long)(numbers[max_index1])) * numbers[max_index2];
+}
 
 int main() {
     int n;
-    std::cin >> n;
-    std::vector<int> numbers(n);
+    cin >> n;
+    vector<int> numbers(n);
     for (int i = 0; i < n; ++i) {
         std::cin >> numbers[i];
     }
 
-    std::cout << MaxPairwiseProduct(numbers); 
+    cout << MaxPairwiseProductFast(numbers); 
     return 0;
 }
