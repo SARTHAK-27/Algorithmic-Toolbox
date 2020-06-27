b37c41891c93bf44f526d33ea44a965ac9965306
SARTHAK-27 <sarthakasthana2014@gmail.com>
2020-06-27 12:32:55 +0530

    week-5 done

---
+++ week-5 Dynamic programming-1/lcs2.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include<algorithm>
+
+using std::vector;
+using std::max;
+
+int lcs2(vector<int> &a, vector<int> &b) {
+    int n = a.size();
+    int m = b.size();
+  vector<vector<int>> dp(n+1, vector<int> (m+1));
+
+  for(int i=0; i<=n; i++){
+      for(int j=0; j<=m; j++){
+          if(i == 0){
+              dp[i][j] = 0;
+          }
+          else if(j == 0){
+              dp[i][j] = 0;
+          }
+          else if(a[i-1] == b[j-1]){
+              dp[i][j] = 1 + dp[i-1][j-1];
+          }
+          else{
+              dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+          }
+      }
+  }
+  return dp[n][m];
+}
+
+int main() {
+  size_t n;
+  std::cin >> n;
+  vector<int> a(n);
+  for (size_t i = 0; i < n; i++) {
+    std::cin >> a[i];
+  }
+
+  size_t m;
+  std::cin >> m;
+  vector<int> b(m);
+  for (size_t i = 0; i < m; i++) {
+    std::cin >> b[i];
+  }
+
+  std::cout << lcs2(a, b) << std::endl;
+}
